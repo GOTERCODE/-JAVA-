41de15aec6814c9b4e45bb02231efac47fd51c47
manic-066g <manic-066g@manic-066>
2024-04-05 16:48:31 +0900

    a

---
+++ project0321/src/Pt0321/Calculator2.java
@@ -0,0 +1,34 @@
+package Pt0321;
+
+public class Calculator2 {
+
+	
+	double areaRectangle(double width) {
+		return width * width;
+	}
+	
+	double areaRectangle(double width, double height) {
+		return width * height;
+	}
+	
+	
+	public static void main(String[] args) {
+		Calculator2 myCalc = new Calculator2();
+		
+		double result1 = myCalc.areaRectangle(4.0);
+		double result2 = myCalc.areaRectangle(4.0, 6.0);
+
+		System.out.println(result1);
+		System.out.println(result2);
+		
+		double A = (result1 * 6);
+		double B = (result2 * 6);
+	
+		System.out.println(A);
+		System.out.println(B);
+	
+	}
+	
+
+
+}
